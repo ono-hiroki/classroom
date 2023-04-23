02124162d77ad4724eee07b92fd805484f56a3ea
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-04-23 17:37:57 +0900

    first commit

---
+++ src/script.js
@@ -0,0 +1,174 @@
+import * as dat from 'lil-gui'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import { OBJLoader} from "three/examples/jsm/loaders/OBJLoader";
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI({
+    width: 400
+})
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Loaders
+ */
+// Texture loader
+const textureLoader = new THREE.TextureLoader()
+
+// Draco loader
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('draco/')
+
+// GLTF loader
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+// 太陽光
+const sunLight = new THREE.DirectionalLight(0xffffff, 1)
+sunLight.position.set(0, 1, 0)
+scene.add(sunLight)
+
+
+/**
+ * Object
+ */
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial()
+)
+
+scene.add(cube)
+/**
+ * Textures
+ */
+const bakedTexture = textureLoader.load('house-bake8.jpg')
+
+/**
+ * Materials
+ */
+// Baked material
+const bakedMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+bakedTexture.flipY = false
+bakedTexture.encoding = THREE.sRGBEncoding
+
+/**
+ * Model
+ */
+// gltfLoader.load(
+//     'house-add-color2.glb',
+//     // 'school_outside2.glb',
+//     (gltf) =>
+//     {
+//         console.log(gltf)
+//         gltf.scene.traverse((child) =>
+//         {
+//             child.material = bakedMaterial
+//         })
+//         scene.add(gltf.scene)
+//     }
+// )
+
+gltfLoader.load(
+    // 'house-add-color2.glb',
+    'school_outside3.glb',
+    (gltf) =>
+    {
+        console.log(gltf.scene)
+        // gltf.scene.scale.set(1000, 1000, 1000)
+        gltf.scene.traverse((child) =>
+        {
+            child.material = bakedMaterial
+        })
+
+        scene.add(gltf.scene)
+    }
+)
+
+const objLoader = new OBJLoader();
+objLoader.load(
+    'kyositu.obj',
+    function (obj) {
+        console.log(obj)
+        scene.add(obj);
+
+    },
+);
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 4
+camera.position.y = 2
+camera.position.z = 4
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.outputEncoding = THREE.sRGBEncoding
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
